0443363ef65ac5cb0bc02866750c36e1e5429475
avadlamani-tech <ashwinvadlamani4@gmail.com>
2025-04-17 16:31:53 -0700

    update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,40 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+        // To recognize Latin script
+        implementation (libs.text.recognition)
+
+        // To recognize Chinese script
+        implementation (libs.text.recognition.chinese)
+
+        // To recognize Devanagari script
+        implementation (libs.text.recognition.devanagari)
+
+        // To recognize Japanese script
+        implementation (libs.text.recognition.japanese)
+
+        // To recognize Korean script
+        implementation (libs.text.recognition.korean)
+
+        //CameraX
+
+        // CameraX core library using the camera2 implementation
+        val camerax_version = "1.5.0-alpha06"
+        // The following line is optional, as the core library is included indirectly by camera-camera2
+        implementation(libs.androidx.camera.core)
+        implementation(libs.androidx.camera.camera2)
+        // If you want to additionally use the CameraX Lifecycle library
+        implementation(libs.androidx.camera.lifecycle)
+        // If you want to additionally use the CameraX VideoCapture library
+        implementation(libs.androidx.camera.video)
+        // If you want to additionally use the CameraX View class
+        implementation(libs.androidx.camera.view)
+        // If you want to additionally add CameraX ML Kit Vision Integration
+        implementation(libs.androidx.camera.mlkit.vision)
+        // If you want to additionally use the CameraX Extensions library
+        implementation(libs.androidx.camera.extensions)
+
+
+
 }
